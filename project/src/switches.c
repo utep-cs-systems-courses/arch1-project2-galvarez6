7a503061fbecae3e8b7a0011e87012e1b161e5ad
Gilbert <galvarez6@miners.utep.edu>
2020-03-22 20:00:00 -0600

    buzzer makes tiny sounds with buttons

--- project/src/switches.c
+++ project/src/switches.c
@@ -1,17 +1,17 @@
 #include <msp430.h>
 #include "switches.h"
-#include "led.h"
+#include "buzzer.h"
 
 char switch_state_down, switch_state_changed; /* effectively boolean */
 
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P2IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P2IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P2IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
 }
 
 void 
@@ -22,14 +22,24 @@ switch_init()			/* setup switch */
   P2OUT |= SWITCHES;		/* pull-ups for switches */
   P2DIR &= ~SWITCHES;		/* set switches' bits for input */
   switch_update_interrupt_sense();
-  led_update();
+  //led_update();
 }
 
 void
 switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
-  switch_state_changed = 1;
-  led_update();
+  char p2val = switch_update_interrupt_sense();
+  //switch_state_down = (p1val & SW0) ? 0 : 1; /* 0 when SW1 is up */
+  //switch_state_changed = 1;
+  //led_update();
+  if(p2val & SW1)
+    buzzer_set_period(100);
+  if(p2val & SW2)
+    buzzer_set_period(1000);
+  if(p2val & SW3)
+    buzzer_set_period(2000);
+  if(p2val & SW4)
+    buzzer_set_period(4000);
+  else
+    buzzer_set_period(0);
 }
