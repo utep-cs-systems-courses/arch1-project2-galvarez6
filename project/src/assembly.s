a997e3bc5d6946cc2c02e245d280d414f2b0c570
Gilbert <galvarez6@miners.utep.edu>
2020-04-09 14:19:15 -0600

    assembly works without range check

--- project/src/assembly.s
+++ project/src/assembly.s
@@ -15,25 +15,25 @@ jt:
 
 	.global nextS
 nextS:
-	cmp #4, &ss		;stay within range for the amount of states
-	jnc default		;if ss is greater than 3 it will jump to default
-	
 	mov &ss, r12		;index in jt
 	add r12, r12		;for referencing the correct case
 	mov jt(r12), r0		;jmp to jt[ss]
 
 s1:	mov #500, r12		;pass parameter for set buzzer period 
 	call #buzzer_set_period	;then call the method
+	mov #2, &ss
 	jmp out			;break
 
 
 s2:	mov #700, r12
 	call #buzzer_set_period
+	mov #3, &ss
 	jmp out
 
 
 s3:	mov #0, r12
 	call #buzzer_set_period
+	mov #1, &ss
 	jmp out
 
 
